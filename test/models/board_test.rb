bbf4bbb81618bfdc9f470b025aa30058a91c3321
Milad Rastian <milad@sam-media.com>
2014-06-17 00:58:32 +0800

    Made MinitTest::Spec work with board test

---
+++ test/models/board_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+
+
+describe Board do
+  it "shouldn't save a board with the same name" do
+    Board.create!(:title => "Foo")
+    lambda {Board.create!(:title => "Foo")}.must_raise(ActiveRecord::RecordInvalid)
+  end
+
+  it "should create 3 list immediately after creation of new board" do
+    board_with_3_lists = Board.create!(:title => "Foo with 3 boards")
+    board_with_3_lists.list.count.must_equal(3)
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,13 +2,14 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
+require 'minitest/autorun'
+require 'minitest/spec'
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
 # require "minitest/rails/capybara"
 
-# Uncomment for awesome colorful output
-# require "minitest/pride"
+require "minitest/pride"
 
 class ActiveSupport::TestCase
     ActiveRecord::Migration.check_pending!

80f3dd3ac537efab112bae21b039091eeb70a88f
Milad Rastian <eslashmili@gmail.com>
2014-06-23 23:35:43 +0800

    Don't use transactional fixtures

--- test/features/can_add_card_to_list_test.rb
+++ test/features/can_add_card_to_list_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 feature "CanAddCardToList" do
+  self.use_transactional_fixtures = false
   background do
     @board_title = 'board_num#' + rand(10000).to_s
     @board = Board.create(title: @board_title)
@@ -9,12 +10,10 @@ feature "CanAddCardToList" do
   scenario "Add new card to lists", js: true do
     Capybara.default_selector = :css
     @board_title = "board_num#{rand(10000).to_s}"
-    @board = Board.new(title: @board_title)
-    @board = Board.find(1)
+    @board = Board.create!(title: @board_title)
     @lists = @board.lists
 
     visit board_path @board
-    sleep 10
     click_link "newCardOpenner#{@lists.first.id.to_s}"
     within "#addCardPopup#{@lists.first.id.to_s}" do
       fill_in 'card_title', with: 'Really Long Text…'
